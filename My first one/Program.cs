c5f8adab2731a068e3af074534f2d3ade6148554
vuish <vuish@DESKTOP-0VJMDSG>
2025-04-17 22:34:09 +0800

    Ew

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
+Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
+Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
+Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
 
