d1137baad14519cea9ea36e05fe3fc5b38934fb1
Cainlen <vuisheongchua@gmail.com>
2025-04-17 22:31:46 +0800

    My first code!

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wisttt hehehehehee!");
+Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
 
