8e94f2eb5bb5c467b34381db4c6110cc7376e952
Cainlen <vuisheongchua@gmail.com>
2025-04-17 22:34:35 +0800

    Test

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
-Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
-Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!"); Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
-
+Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
