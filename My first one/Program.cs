5b76b969052fe7b01819eac674417bd8a9fdc48a
vuish <vuish@DESKTOP-0VJMDSG>
2025-04-17 22:45:28 +0800

    First bracnh commit

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -2,5 +2,14 @@
 Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
 
 void Mymethod()
-
+{
     Console.WriteLine("Lovely Day init");
+}
+void Mymethod()
+{
+    Console.WriteLine("Sunny Day");
+}
+void Mymethod()
+{
+    Console.WriteLine("Semi windy day");
+}
