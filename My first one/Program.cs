e700376c9445fb3278218850fb1434c79c40cf46
vuish <vuish@DESKTOP-0VJMDSG>
2025-04-17 22:41:10 +0800

    Its a way

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
 
+void Mymethod()
+
+    Console.WriteLine("Lovely Day init");
