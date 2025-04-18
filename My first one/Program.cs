af4306379341a4b24b12c00439276777b451f45a
vuish <vuish@DESKTOP-0VJMDSG>
2025-04-18 10:17:12 +0800

    Changes

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,15 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
 
-void Mymethod()
-{
-    Console.WriteLine("Lovely Day init");
-}
-void Mymethod()
-{
-    Console.WriteLine("Sunny Day");
-}
-void Mymethod()
-{
-    Console.WriteLine("Semi windy day");
-}
+void Mymethod() => Console.WriteLine("Lovely Day init");
+void Mymethod() => Console.WriteLine("Sunny Day");
+void Mymethod() => Console.WriteLine("Semi windy day");
