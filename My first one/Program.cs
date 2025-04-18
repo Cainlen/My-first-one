43afffa9bdbafcddf8ee48ef1ddc1e77b3f109c3
vuish <vuish@DESKTOP-0VJMDSG>
2025-04-18 11:13:03 +0800

    Test 1

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,10 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
+﻿// See https://aka.ms/new-console-template for more information  
+//Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");  
 
-void MymethodLovelyDay() => Console.WriteLine("Lovely Day init");
-void MymethodSunnyDay() => Console.WriteLine("Sunny Day");
-void MymethodSemiWindyDay() => Console.WriteLine("Semi windy day");
+Console.WriteLine("Lovely Day init");
+Console.WriteLine("Sunny Day");
+Console.WriteLine("Semi windy day");
 
-MymethodLovelyDay();
-MymethodSunnyDay();
-MymethodSemiWindyDay();
+char userOption = 'A';
+Console.WriteLine($"User option: {userOption}");
+
+int gameScore = 100;
+Console.WriteLine($"Game score: {gameScore}");
+
+decimal particlesPerMillion = 0.0000001m;
+Console.WriteLine($"Particles per million: {particlesPerMillion}");
+
+bool processedCustomer = true;
+bool isProcessed = true;
+Console.WriteLine($"Processed customer: {processedCustomer}");
+Console.WriteLine($"Is processed: {isProcessed}");
+
+Console.WriteLine("Sounds");
+Console.WriteLine(2500);
+Console.WriteLine(12.325263321m);
+Console.Write(true);
+Console.Write(" "); Console.Write("or"); Console.Write(" "); Console.Write(false);
