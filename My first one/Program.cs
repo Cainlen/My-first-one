44151582e9a0113505238c147def4b04ab87d0be
vuish <vuish@DESKTOP-0VJMDSG>
2025-04-18 10:18:30 +0800

    Changes wtih copilot

--- My first one/Program.cs
+++ My first one/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Wisttt hehehehehee its my first code!!!");
 
-void Mymethod() => Console.WriteLine("Lovely Day init");
-void Mymethod() => Console.WriteLine("Sunny Day");
-void Mymethod() => Console.WriteLine("Semi windy day");
+void MymethodLovelyDay() => Console.WriteLine("Lovely Day init");
+void MymethodSunnyDay() => Console.WriteLine("Sunny Day");
+void MymethodSemiWindyDay() => Console.WriteLine("Semi windy day");
+
+MymethodLovelyDay();
+MymethodSunnyDay();
+MymethodSemiWindyDay();
